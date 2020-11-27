159eb6d6cb24cc4fe88f511e9cd3dea17e4b95f9
Juan Duarte <juan.duarte.a@gmail.com>
2020-11-27 01:24:50 -0500

    Overall CommInterface code and communication protocol optimized
    'send_data' method no longer send responses unless test parameter 'true'.
    CommInterface has a new dictionary update automatically from server with state parameters.
    Module code optimization:
    - Int/Byte/Bool conversion methods eliminated.
    - 'is_oriented', 'front_distance', and 'center_distance' methods eliminated.

--- comm_interface/ml_comm.py
+++ comm_interface/ml_comm.py
@@ -55,15 +55,23 @@ class CommInterface:
     server_latency: float
     verbose: bool
     input_comm_handler: threading.Thread
-    output_data_size: int = 3
-    input_data_size: int = 10
+    OUTPUT_DATA_SIZE: int = 3
+    INPUT_DATA_SIZE: int = 50
+    state_parameters: dict
 
     def __init__(self, latency: float = 0.005, verbose_option: bool = False):
         self.output_port = 11435
+        self.input_port = 11436
         self.connected = False
         self.start_timestamp = time.time()
         self.server_latency = latency
         self.verbose = verbose_option
+        self.state_parameters = {
+            "is_oriented": True,
+            "distance_from_center": 0,
+            "front_distance": 0,
+            "track_completion": 0.0
+        }
 
     def connect(self, output_port_number: int = 11435, input_port_number: int = 11436) -> None:
         """
@@ -93,10 +101,11 @@ class CommInterface:
             print("Esperando conexión de salida con ML Racing...")
             time.sleep(1.0)
 
+        print("Iniciando transmisión de datos por conexión de salida.")
         connection_check = self.send_action(self.CONNECTION_OK)
-        if connection_check[0] == Errors.OK:
+        if connection_check is not None and connection_check[0] == Errors.OK:
             print("Conexión de salida establecida.")
-        elif connection_check[0] == Errors.CONNECTION_ERROR:
+        elif connection_check is not None and connection_check[0] == Errors.CONNECTION_ERROR:
             print("¡Error estableciendo conexión de salida!")
             return
 
@@ -110,52 +119,16 @@ class CommInterface:
             time.sleep(1.0)
 
         if len(self.client_address_receive) > 0:
-            self.connected = True
             print("Conexión de entrada establecida.")
             print("Conexión con ML Racing completada.")
+            self.connected = True
             self.input_comm_handler = threading.Thread(target=self._input_data_handler)
             self.input_comm_handler.start()
-            time.sleep(0.5)
+            time.sleep(0.6)
             if self.verbose:
                 print("Continuando ejecución del hilo principal.")
         else:
             print("¡Error estableciendo conexión de entrada!")
-
-    def _connect_request_completed(self, address: tuple) -> bool:
-        try:
-            self.output_socket.connect(address)
-            return True
-        except ConnectionRefusedError:
-            return False
-
-    def _connection_wait_completed(self) -> bool:
-        try:
-            self.client_address_receive = self.input_socket.accept()
-            return True
-        except ConnectionRefusedError:
-            return False
-
-    def _get_bool(self, data: bytes) -> bool:
-        return True if data[2] == self.BYTE_TRUE else False
-
-    @staticmethod
-    def _get_int(data: bytes) -> int:
-        if int(data[1] / 128) == 0:
-            return -1 * (data[1] * 256 + data[2])
-        else:
-            return (data[1] - 128) * 256 + data[2]
-
-    @staticmethod
-    def _int_byte_code(int_value: int) -> list:
-        int1: int
-        int2: int
-
-        int1 = int(abs(int_value) / 256)
-        if int_value > 0:
-            int1 += 128
-        int2 = abs(int_value) - (int1 if int1 < 128 else (int1 - 128)) * 256
-
-        return [int1, int2]
 
     def communicate_test(self, iterations: int = 1) -> None:
         """
@@ -166,65 +139,23 @@ class CommInterface:
         """
         data_sent: bytearray
         data_received: list
-        # Send data
-        # input("Start [Enter] ") # Manual activation of the communication protocol.
         for i in range(iterations):
             data_sent = bytearray(bytes([65, 66, 67]))
-            # data_sent.clear() # bytearray must be empty before sending it.
-            # data_sent.extend(map(ord, data_string)) # str to bytearray.
             if self.verbose:
                 print("Iteración:", i)
-            self.send_action(data_sent)
+            self.send_action(data_sent, True)
 
-    def distance_from_center(self) -> list:
-        """
-        Retorna la distancia a la que el vehículo se encuentra del centro de la pista.
-
-        :return:
-            'list' de dos valores: entero con el código de error y resultado.
-            Si el vehículo se encuentra por fuera de la pista el resultado será 0.
-        """
-
-        result: list = self.send_action(self.CENTER_DISTANCE)
-        distance: int
-
-        if result[0] == Errors.OK:
-            distance = self._get_int(result[1])
-            return [Errors.OK, distance]
-        elif result[0] == Errors.BUSY:
-            return Errors.get_error(Errors.BUSY)
-        else:
-            return Errors.get_error(Errors.DEFAULT_ERROR)
-
-    def is_oriented(self) -> list:
-        """
-        Verifica la orientación del vehículo de acuerdo al sentido de la pista.
-
-        :return:
-            'list' de dos valores: entero con el código de error y resultado.
-            El resultado será True si el vehículo se encuentra correctamente orientado, False de lo contrario.
-            Si el vehículo se encuentra por fuera de la pista el resultado será False.
-        """
-
-        result: list = self.send_action(self.IS_ORIENTED)
-        oriented: bool
-
-        if result[0] == Errors.OK:
-            oriented = self._get_bool(result[1])
-            return [Errors.OK, oriented]
-        elif result[0] == Errors.BUSY:
-            return Errors.get_error(Errors.BUSY)
-        else:
-            return Errors.get_error(Errors.DEFAULT_ERROR)
-
-    def send_action(self, action: bytearray) -> list:
+    def send_action(self, action: bytearray, response_test: bool = False) -> list:
         """
         Envía una acción al vehículo.
 
         :param action:
             Acción a ejecutar de acuerdo a las constantes definidas en la clase Actions de este módulo.
+        :param response_test:
+            Ejecuta el método en modo de prueba. Dejar en 'False' (valor por defecto) para uso normal.
         :return:
-            'list' con dos valores: código de error y resultado.
+            'None', en modo normal.
+            'list', en modo de prueba, con dos valores: código de error y resultado.
         """
 
         result: list = []
@@ -237,7 +168,6 @@ class CommInterface:
             if self.verbose:
                 print("Servidor de salida ocupado. - Err:", Errors.BUSY, "\nIntentando de nuevo...")
             time.sleep(self.server_latency - wait_time)
-            # return Errors.get_error(Errors.BUSY)
         else:
             self.start_timestamp = time.time()
 
@@ -257,38 +187,24 @@ class CommInterface:
         try:
             bytes_sent = self.output_socket.send(action)
             if self.verbose:
-                print("Enviado ->", action.decode(), _message, "- bytes:", bytes_sent)
+                print("Enviado ->", action.decode(), _message, "- Bytes:", bytes_sent)
 
-            data_received = bytes()
-            while action != Actions.END and len(data_received) == 0:
-                data_received = self.output_socket.recv(self.output_data_size)
-                if len(data_received) == 0:
-                    time.sleep(self.server_latency)
+            if response_test:
+                data_received = bytes()
+                while action != Actions.END and len(data_received) == 0:
+                    data_received = self.output_socket.recv(self.OUTPUT_DATA_SIZE)
+                    if len(data_received) == 0:
+                        time.sleep(self.server_latency)
 
-            result = [0, data_received]
+                result = [0, data_received]
 
-            if self.verbose:
-                print("Recibido <-", data_received)
+                if self.verbose:
+                    print("Recibido <-", data_received)
+            time.sleep(self.server_latency)
         except AttributeError:
             result = Errors.get_error(Errors.CONNECTION_ERROR)
         finally:
-            return result
-
-    def _input_data_handler(self) -> None:
-        data_received: bytes
-        conn = self.client_address_receive[0]
-        time.sleep(0.5)
-        data_received = conn.recv(self.input_data_size)
-        if self.verbose:
-            print("Transmisión de datos de conexión de entrada <-", data_received.decode())
-
-        while self.connected:
-            data_received = bytes()
-            while len(data_received) == 0:
-                data_received = conn.recv(self.input_data_size)
-                time.sleep(self.server_latency * 10)
-            if self.verbose:
-                print("Recibido de conexión de entrada <-", data_received.decode())
+            return result if response_test else None
 
     def close_comm(self) -> None:
         """
@@ -296,4 +212,51 @@ class CommInterface:
         """
 
         self.output_socket.close()
+        self.input_socket.close()
         self.connected = False
+
+    def _connect_request_completed(self, address: tuple) -> bool:
+        try:
+            self.output_socket.connect(address)
+            return True
+        except ConnectionRefusedError:
+            return False
+
+    def _connection_wait_completed(self) -> bool:
+        try:
+            self.client_address_receive = self.input_socket.accept()
+            return True
+        except ConnectionRefusedError:
+            return False
+
+    def _input_data_handler(self) -> None:
+        message: str
+        message_length: bytes
+        data_received: bytes = bytes()
+        parameters: list
+        conn = self.client_address_receive[0]
+
+        if self.verbose:
+            print("Iniciando hilo secundario...")
+        while len(data_received) == 0:
+            data_received = conn.recv(self.INPUT_DATA_SIZE)
+
+        if self.verbose:
+            print("Transmisión de datos de conexión de entrada <-", data_received.decode())
+
+        while self.connected:
+            data_received = bytes()
+            while len(data_received) == 0:
+                message_length = conn.recv(1)
+                data_received = conn.recv(message_length[0])
+            message = data_received.decode()
+            if data_received[0] == 76 and data_received[1] == 84:
+                if self.verbose:
+                    print("Recibido <-", data_received.decode())
+            else:
+                parameters = message.split(":")
+                self.state_parameters["is_oriented"] = str(parameters[0]).lower() == "true"
+                self.state_parameters["distance_from_center"] = int(parameters[1])
+                self.state_parameters["front_distance"] = int(parameters[2])
+                self.state_parameters["track_completion"] = float(parameters[3])
+            time.sleep(self.server_latency * 2)
